635a8f3f7008b603072bc8599450fd8f88c3922f
Sarius23 <jacob.zahn@gmx.de>
2026-09-21 14:52:37 +0200

    added tmep resistor values [test]

--- include/config.h
+++ include/config.h
@@ -92,7 +92,7 @@ constexpr float MAX31865_RREF[MAX31865_CHANNEL_COUNT] =
 {
     4291.0f, 4295.0f, 4302.0f,
     4311.0f, 4290.0f, 4306.0f,
-    4296.0f, 4296.0f, 4292.0f
+    4296.0f, 4296.0f, 4292.1f
 };
 // T_calibrated = T_measured * scale + offset.
 constexpr float MAX31865_SCALE[MAX31865_CHANNEL_COUNT] =
